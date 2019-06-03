261107d7d138af98d1ff768699705853fcd418b0
知久 秀康 <h-chiku@virtualex.local>
2019-06-03 13:00:02 +0900

    I1.(i)clear

--- CHIKU_Hideyasu_I1.py
+++ CHIKU_Hideyasu_I1.py
@@ -1,1 +1,18 @@
 import numpy as np
+
+# (i) create matrix
+with open("iris.data","r") as fo:
+    list = np.array([])
+    for line in fo:
+        number = line.split(",")
+        del number[-1]
+        if len(number) != 0 :
+            # print(number)
+            FloatNumber = [float(s) for s in number]
+            list = np.append(list,FloatNumber)
+
+list = list.reshape(150,4)
+list = list.T
+print(list)
+
+# (ii) count the number of specified lines
--- study.py
+++ study.py
@@ -7,7 +7,7 @@ print(a['age'])
 
 student = np.dtype([('name','S20'), ('age', 'i1'), ('marks', 'f4')])
 a = np.array([('abc', 21, 50),('xyz', 18, 75)], dtype = student)
-print(a['name'])
+print(a['age'])
 
 a = np.array([[1,2,3],[4,5,6]])
 print(a.shape)
@@ -35,3 +35,51 @@ a = np.array([1,2,3,4],dtype=np.int8)
 print(a)
 
 print(a.itemsize)
+
+print(a.flags)
+
+b = a
+
+print(b.flags)
+
+x = np.empty([3,5],dtype=int)
+print(x)
+
+x = np.zeros([3,5],dtype=int)
+print(x)
+
+x = np.zeros((3,5),dtype=[('x','i1'),('y','i4'),('z','i4')])
+print(x)
+
+s = 'Hello World1'
+a = np.frombuffer(s, dtype = 'S3')
+print(a)
+
+
+a = np.array([[3,7],[9,1]])
+
+print(a)
+
+print(np.sort(a, axis = 0))
+
+
+
+a = np.array([[30,0,40],[0,20,10],[50,0,0]])
+
+print 'Our array is:'
+print a
+print '\n'
+
+print 'Applying nonzero() function:'
+print np.nonzero (a)
+
+a = np.array([1,2,3,4,5])
+np.save('outfile',a)
+
+b = np.load('outfile.npy')
+print b
+
+a = np.array([1,2,3,4,5])
+np.savetxt('out.txt',a)
+b = np.loadtxt('out.txt')
+print b 
