9bebf6e3a00222808c7e43ca497e45a9c86bd474
知久 秀康 <h-chiku@virtualex.local>
2019-06-03 14:53:19 +0900

    I1 success

--- CHIKU_Hideyasu_I1.py
+++ CHIKU_Hideyasu_I1.py
@@ -12,7 +12,40 @@ with open("iris.data","r") as fo:
             list = np.append(list,FloatNumber)
 
 list = list.reshape(150,4)
-list = list.T
-print(list)
+print("(i)\n",list,"\n")
 
 # (ii) count the number of specified lines
+cnt = 0
+for num in list:
+    if num[0] > 5:
+        cnt += 1
+
+print("(ii)\n",cnt,"\n")
+
+
+# (iii) report the average
+multi = np.array([])
+for num in list:
+    multi = np.append(multi,num[0]*num[1])
+
+avg = multi.mean()
+print("(iii)\n",avg,"\n")
+
+# (iv) what is the std
+print("(iv)\n",np.std(list,axis=0)[3],"\n")
+
+# (v) find specified rows
+ans = np.array([])
+cnt = 0
+for num in list:
+    if num[2]*num[3] > 1.0:
+        ans = np.append(ans,num)
+        cnt += 1
+
+ans = ans.reshape(cnt,4)
+print("(v)\n",ans) 
+
+np.save("small_petal",ans)
+
+# b = np.load("small_petal.npy")
+# print(b)
